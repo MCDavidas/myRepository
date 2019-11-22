c319969f97b56d43dbb67a8630ad38c2fc491a79
Davidas <buildcommunism@gmail.com>
2019-11-23 01:28:00 +0300

    Fix

--- gamejs/script.js
+++ gamejs/script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("topCanvas");
+var canvas = document.getElementById("MyCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 8;
 var paddleHeight = 10;
